7fc36ed214cd4183fca6ecdb6ce80b8f15ba73e8
Ron Johnson <github@iti.technology>
2019-05-27 11:04:17 +0100

    feat: use computer independent path
    use $PWD to correctly locate map and yaml files.

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 echo "    roslaunch turtlebot_gazebo turtlebot_world.launch ..."
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/ros/ros/home-service-robot/src/basketbot/world/RoboWorld2.world
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$PWD/src/basketbot/world/RoboWorld2.world
 " &
 sleep 5
 echo "    roslaunch turtlebot_gazebo amcl_demo.launch ..."
-xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/ros/ros/home-service-robot/src/basketbot/maps/map.yaml" &
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$PWD/src/basketbot/maps/map.yaml" &
 sleep 5
 echo "    roslaunch turtlebot_rviz_launchers view_navigation.launch ..."
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
